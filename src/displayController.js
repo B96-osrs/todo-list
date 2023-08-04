6fa3ffb35b66781c2a439fa142099d39efabdd9e
B96-osrs <115960201+B96-osrs@users.noreply.github.com>
2023-08-04 17:56:53 +0300

    Add function to display project header

--- src/displayController.js
+++ src/displayController.js
@@ -1,43 +1,67 @@
 const displayController = (function() {
 
     function displayProjectList(parent, projectArray) {
-        projectArray.forEach(element => {
+        for(let i = 0; i < projectArray.length; i++) {
             let projectDiv = document.createElement("div");
-            projectDiv.textContent = element.name;
+            projectDiv.classList.add("project");
+            projectDiv.setAttribute("data-key",i);
+            projectDiv.textContent = projectArray[i].name;
             parent.appendChild(projectDiv);
 
-        });
+        }
     }
 
-    function displayTodoItems(parent,todoArray) {
-        todoArray.forEach(element => {
+    function displayTodoItems(parent, project) {
+        project.todoArray.forEach(element => {
             let todoCard = document.createElement("div");
             todoCard.classList.add("todo-card");
-
-            let todoTitle = document.createElement("div");
-            todoTitle.textContent = element.title;
-
-            let todoDescription = document.createElement("div");
-            todoDescription.textContent = element.description;
-
-            let todoDueDate = document.createElement("div");
-            todoDueDate.textContent = element.dueDate;
-            let todoPriority = document.createElement("div");
-            todoPriority.textContent = element.priority;
-            let todoCheckbox = document.createElement("div");
-            todoCheckbox.textContent = element.checkBox;
-
             parent.appendChild(todoCard);
-            todoCard.appendChild(todoTitle);
-            todoCard.appendChild(todoDescription);
-            todoCard.appendChild(todoDueDate);
-            todoCard.appendChild(todoPriority);
-            todoCard.appendChild(todoCheckbox);
+
+            createTodoElements(todoCard,element);
 
         });
     }
 
-    return {displayProjectList, displayTodoItems};
+
+    function createTodoElements(parent,element) {
+        let todoTitle = document.createElement("div");
+        todoTitle.textContent = element.title;
+
+        let todoDescription = document.createElement("div");
+        todoDescription.textContent = element.description;
+
+        let todoDueDate = document.createElement("div");
+        todoDueDate.textContent = element.dueDate;
+        let todoPriority = document.createElement("div");
+        todoPriority.textContent = element.priority;
+        let todoCheckbox = document.createElement("div");
+        todoCheckbox.textContent = element.checkBox;
+
+        parent.appendChild(todoTitle);
+        parent.appendChild(todoDescription);
+        parent.appendChild(todoDueDate);
+        parent.appendChild(todoPriority);
+        parent.appendChild(todoCheckbox);
+    }
+
+    function displayProjectHeader(parent, project) {
+        let projectHeader = document.createElement("div");
+        parent.appendChild(projectHeader);
+
+        let projectName = document.createElement("div");
+        projectName.classList.add("project-title");
+        projectName.textContent = project.name;
+
+        projectHeader.appendChild(projectName);
+
+    }
+
+    function clearCurrentProject(parent) {
+        parent.innerHTML = "";
+    }
+
+
+    return {displayProjectList, displayTodoItems, displayProjectHeader,clearCurrentProject};
 })();
 
 
--- src/index.js
+++ src/index.js
@@ -19,9 +19,30 @@ projectArray[0].todoArray[0] = toDo("Homework","Do the coding assignment from TO
 projectArray[0].todoArray[1] = toDo("Reading","Read article xzy", "15.11.2024","low");
 projectArray[0].todoArray[2] = toDo("Airport pickup","Pick up Odin from the airport", "11.08.2023","high");
 
+projectArray[1].todoArray[0] = toDo("zzz","zzzzzzzz", "16.07.2023","high");
+projectArray[1].todoArray[1] = toDo("Reading","Read article xzy", "15.11.2024","low");
+projectArray[1].todoArray[2] = toDo("Airport pickup","Pick up Odin from the airport", "11.08.2023","high");
+
+projectArray[2].todoArray[0] = toDo("uuuuuu","uuuuuuuuuu", "17.07.2023","low");
+projectArray[2].todoArray[1] = toDo("Reading","Read article xzy", "15.11.2024","low");
+projectArray[2].todoArray[2] = toDo("Airport pickup","Pick up Odin from the airport", "11.08.2023","high");
+
 displayController.displayProjectList(projectListBox, projectArray);
-displayController.displayTodoItems(mainContainer,projectArray[0].todoArray);
+displayController.displayProjectHeader(mainContainer,projectArray[0]);
+displayController.displayTodoItems(mainContainer,projectArray[0]);
+
 
 projectArray[0].todoArray.forEach(element => {
     console.log(element);
+});
+
+window.addEventListener("click",function(e) {
+    if(e.target.matches(".project")) {
+        let num = parseInt(e.target.dataset.key);
+        displayController.clearCurrentProject(mainContainer);
+        displayController.displayProjectHeader(mainContainer,projectArray[num]);
+        displayController.displayTodoItems(mainContainer,projectArray[num]);
+    }
+
+
 });
